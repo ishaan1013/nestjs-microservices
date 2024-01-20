06e0feed844fbcc1e35e62718e857858fbd318a8
Ishaan Dey <ishaan1013@gmail.com>
2024-01-20 17:29:15 -0500

    Finish implementation

--- analytics/src/main.ts
+++ analytics/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port: 3001,
+    },
+  });
+  await app.startAllMicroservices();
+  // sample backend is already running on port 3000
+  await app.listen(3001);
 }
 bootstrap();
